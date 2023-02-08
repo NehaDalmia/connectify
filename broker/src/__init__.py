a91979c32656fd0160183c2dd8a8e742a01bfbaa
Rajat Bachhawat <rbachhawat.96@gmail.com>
2023-02-08 13:53:13 +0530

    Add DB_NAME env variable to broker

--- broker/src/__init__.py
+++ broker/src/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 from src.json_validator import expects_json
 from flask_sqlalchemy import SQLAlchemy
 import config
-import os
 
 app = Flask(__name__)
 app.config.from_object(config.ProdConfig)
@@ -11,6 +10,6 @@ db = SQLAlchemy(app)
 
 
 with app.app_context():
-    db.drop_all()
+    db.create_all()
 
 from src import views
