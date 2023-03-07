e3f51b1933135a5a906f624cbd3a9e3178c1fce4
Nisarg Upadhyaya <nisarg1631@gmail.com>
2023-03-07 23:58:45 +0530

    complete async requests

--- primary_manager/src/sync_utils.py
+++ primary_manager/src/sync_utils.py
@@ -1,6 +1,7 @@
 import requests
 import aiohttp
 import os
+from typing import Dict, Any
 
 from src import AsyncRequests
 
@@ -10,19 +11,19 @@ async_request_handler = AsyncRequests()
 async def _sync_broker_metadata(
     session: aiohttp.client.ClientSession, 
     route: str, 
-    broker_host: str, 
     read_manager_index: int,
-    project_name: str
+    project_name: str,
+    json_data: Dict[str, Any]
 ):
     url = f"http://{project_name}-readonly_manager-{read_manager_index+1}:5000{route}"
-    async with session.post(url, json={"broker_host":broker_host}) as response:
+    async with session.post(url, json=json_data) as response:
         response_status = response.status
         response_json = await response.json()
         return response_status, response_json
 
 def sync_broker_metadata(
     route: str, 
-    broker_host: str
+    json_data: Dict[str, Any]
 ):
     read_only_count = int(os.environ["READ_REPLICAS"])
     project_name = os.environ["COMPOSE_PROJECT_NAME"]
@@ -31,7 +32,7 @@ def sync_broker_metadata(
         [
             {
                 "route": route, 
-                "broker_host": broker_host, 
+                "json_data": json_data, 
                 "read_manager_index": read_manager_index,
                 "project_name": project_name
             } for read_manager_index in range(read_only_count)
